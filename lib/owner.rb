d650cdda4d9d532257759719893623d20186cee1
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-26 18:50:04 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -47,5 +47,35 @@ class Owner
     end
   end
 
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
 
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets[:dogs].each do |dog|
+      dog.mood = "nervous"
+    end
+    @pets[:dogs] = []
+    @pets[:cats].each do |cat|
+      cat.mood = "nervous"
+    end
+    @pets[:cats] = []
+    @pets[:fishes].each do |fish|
+      fish.mood = "nervous"
+    end
+    @pets[:fishes] = []
+  end
+
+  def list_pets
+    return "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+  end
+    
 end
