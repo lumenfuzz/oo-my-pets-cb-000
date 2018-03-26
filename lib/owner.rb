595d735828b072ac07c1298c29bbb2c05571b54a
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-26 13:58:25 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
-  # code goes here
+  attr_reader :species, :pets
+  attr_accessor :name
+  @@all = []
+
+  def initialize(species)
+    @species = species
+  end
+
+  def self.count
+    return @@all
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def buy_fish(name)
+    @fish = fish
+  end
+
+  def buy_cat(name)
+  end
+
+  def buy_dog(name)
+  end
+  
 end
